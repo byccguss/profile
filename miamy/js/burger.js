f45c1de7ded8a6be275d374bc326cde36b16d7ad
seva <115292975+byccguss@users.noreply.github.com>
2023-03-23 13:11:54 +0200

    Add files via upload

---
+++ miamy/js/burger.js
@@ -0,0 +1,26 @@
+// $(document).ready(function(){
+// 	$('.header__burger').click(function(event){
+// 		$('.header__burger, .header__menu').toggleClass('active');
+// 		$('body').toggleClass('lock')
+// 	});
+// 	$('.header__phone').click(function(event){
+// 		$('.header__phone-number').toggleClass('number');
+// 	});
+// });
+
+// // Навигационый пункт
+
+// function scrollNav() {
+// 	$('.page__navigation a').click(function(){
+// 	//   $(".active").removeClass("active");     
+// 	//   $(this).addClass("active");
+	  
+// 	  $('html, body').stop().animate({
+// 		 scrollTop: $($(this).attr('href')).offset().top - 160
+// 	  }, 300);
+// 	  return false;
+// 	});
+//  }
+//  scrollNav();
+
+//  // Навигационый пункт
